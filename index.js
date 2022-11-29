ae7ae09ace2ef26889a5a793f48ed73c91bd3cc5
Adnan <77362547+adddeveloper@users.noreply.github.com>
2022-11-29 08:09:46 -0400

    n

---
+++ index.js
@@ -0,0 +1,65 @@
+var ddata;
+var mssg__ = document.getElementById("mssg__");
+
+fetch("data/you.json")
+.then (res=>res.json())
+.then(data =>{ddata = data})
+.then(()=>{
+    if(window.location.href.split("#")[1]){
+        mssg__.innerHTML = ddata[parseInt(window.location.href.split("#")[1])];
+    } else {
+        var randnum = Math.floor(Math.random()* ddata.length)
+        mssg__.innerHTML = ddata[randnum];
+    }
+})
+
+function img__() {
+    var a = document.createElement("a");
+    a.href = "https://www.adnans.ml";
+    a.target = "_blank";
+    a.click();
+}
+
+function contact__(){
+    var a = document.createElement("a");
+    a.href = "https://www.adnans.ml/contact-me/";
+    a.target = "_blank";
+    a.click();
+}
+
+var items = document.querySelectorAll("div.cursosr-pointer");
+
+function link__() {
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    (window.location.href)
+    navigator.clipboard.writeText();
+}
+
+function email__() {
+    
+}
+
+function twitter__(){
+
+}
+
+items.forEach((e,i)=>{
+    switch(i){
+        case 0:
+            twitter__()
+            console.log("twitter")
+            break
+        case 1:
+            email__()
+            console.log("email")
+            break
+        case 2:
+            link__()
+            console.log("link")
+            break
+    }
+})
