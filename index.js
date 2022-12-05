741961251d093fde8eafe78de314fc1137799e3e
Adnan <77362547+adddeveloper@users.noreply.github.com>
2022-12-04 22:27:54 -0400

    n

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
+var whatcoppied__ = document.getElementById("whatcoppied__");
+var date__ = document.querySelector(".date__");
+var coppiedEl = document.getElementById("coppied");
+coppiedEl.children[0].children[0].addEventListener("click", ()=>{
+    if (!coppiedEl.classList.contains("d-none")) {
+        coppiedEl.classList.toggle("d-none");
+    }
+})
+
 var ddata;
 var mssg__ = document.getElementById("mssg__");
-var randnum;
+var randnum = parseInt(window.location.href.split("#")[1]);
+
+function isInt(value) {
+  return (
+    !isNaN(value) &&
+    parseInt(Number(value)) == value &&
+    !isNaN(parseInt(value, 10))
+  );
+}
 
 fetch("data/you.json")
 .then (res=>res.json())
 .then(data =>{ddata = data})
 .then(()=>{
-    if(window.location.href.split("#")[1]){
+    var time__ = new Date();
+    function formatAMPM(date) {
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
+    date__.innerHTML = formatAMPM(time__) +" - "+ String(time__).split(" ")[1]+" "+String(time__).split(" ")[2]+", "+String(time__).split(" ")[3];
+
+    if(isInt(window.location.href.split("#")[1])){
         mssg__.innerHTML = ddata[parseInt(window.location.href.split("#")[1])];
     } else {
-        randnum = Math.floor(Math.random()* ddata.length)
+        randnum = Math.floor(Math.random()* ddata.length);
         mssg__.innerHTML = ddata[randnum];
     }
 })
@@ -31,36 +61,45 @@ function contact__(){
 var items = document.querySelectorAll("div.cursosr-pointer");
 
 function link__() {
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-
-    // Copy the text inside the text field
-    var ccpy = window.location.href + "#" + randnum;
-    navigator.clipboard.writeText(ccpy);
+    var cppy = String(window.location.href+"#"+randnum);
+    navigator.clipboard.writeText(cppy);
+    whatcoppied__.innerHTML = cppy;
+    if (coppiedEl.classList.contains("d-none")) {
+        coppiedEl.classList.toggle("d-none");
+    }
+    setTimeout(()=>{
+        if (!coppiedEl.classList.contains("d-none")) {
+            coppiedEl.classList.toggle("d-none");
+        }  
+    }, 5000)
 }
 
 function email__() {
-    
+    var a = document.createElement("a");
+    var body__ = "Hello\nI saw this and I think you need to see it:\n" + window.location.href+"#"+randnum;
+    a.href = "mailto:?subject=&body="+encodeURIComponent(body__);
+    a.target = "_blank";
+    a.click();
 }
 
 function twitter__(){
-
+    var a = document.createElement("a");
+    var body__ = "Hello\nI saw this and I think you need to see it:\n" + window.location.href+"#"+randnum;
+    a.href = "https://twitter.com/intent/tweet?text="+encodeURIComponent(body__);
+    a.target = "_blank";
+    a.click();
 }
 
 items.forEach((e,i)=>{
     switch(i){
         case 0:
-            twitter__()
-            console.log("twitter")
+            e.addEventListener("click", ()=>{twitter__()});
             break
         case 1:
-            email__()
-            console.log("email")
+            e.addEventListener("click", ()=>{email__()});
             break
         case 2:
-            e.addEventListener("click", link__());
-            console.log("link")
+            e.addEventListener("click", ()=>{link__()});
             break
     }
 })
